21ee560e98cc777af3b82447d106d10f1b327720
Sebastian Soto <elsebasoto@gmail.com>
2025-11-15 00:42:43 +0000

    delete module1

--- module1/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["module1/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- module1/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"module1/test/unit/controller/Home.controller"
-], function () {
-	"use strict";
-});
